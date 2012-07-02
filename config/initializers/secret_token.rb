210886a4e948fce0894e7971694eeb81bb26ee7b
Dan <dkissell@gmail.com>
2012-07-02 13:41:21 -0400

    initial rails/refinery setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpiSabers::Application.config.secret_token = '014b9f9ec68a4da74fd60c936bddb77bbd2a573e6213e5f700b557521c93b988852aed448f67991056afe79359c71e8b56ad9820c207d75892e4a7f433c5d460'
